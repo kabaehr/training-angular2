db4c50064d490aa82df01e686c6ccdc3009398ef
stanbe <stefan.benz@zuehlke.com>
2016-10-18 13:29:31 +0200

    reintroduce nav menu component

--- Application/Zuehlke.ExpenseReporting/ClientApp/app/components/app/app.component.ts
+++ Application/Zuehlke.ExpenseReporting/ClientApp/app/components/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: require('./app.component.html')
+    template: require('./app.component.html'),
+    styles: [require('./app.component.css')]
 })
-
 export class AppComponent {
-    pageTitle: string = 'Expenses Management Tool';
 }
+
--- Application/Zuehlke.ExpenseReporting/ClientApp/app/components/home/home.component.ts
+++ Application/Zuehlke.ExpenseReporting/ClientApp/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template: require('./welcome.component.html')
+    selector: 'home',
+    template: require('./home.component.html')
 })
-export class WelcomeComponent {
-    public pageTitle: string = 'Welcome';
+export class HomeComponent {
     public zuehlkeLogo: string = require('../../assets/images/zuehlke_logo.jpg');
 }
