4afc960a93ba55844c2f3bcf38fe3609c6bf584e
stanbe <stefan.benz@zuehlke.com>
2016-10-18 11:46:41 +0200

    add first two tests

---
+++ Application/Zuehlke.ExpenseReporting/ClientApp/app/components/expense/expense-filter.pipe.spec.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../../../typings/globals/jasmine/index.d.ts" />
+
+import { ExpenseFilterPipe } from './expense-filter.pipe';
+import { Expense, Reason } from './expense';
+
+describe('TitleCasePipe', () => {
+   let pipe = new ExpenseFilterPipe();
+
+   it('transforms "abc" to "Abc"', () => {
+        let expense1 = new Expense('1', 'Anakin Skywalker', Reason.Hotel, "12-01-2016", 12.20, "Night on Coruscant with Obi Wan");
+        let expense2 = new Expense('1', 'Obi Wan', Reason.Hotel, "12-01-2016", 12.20, "Night on Coruscant with Anakin");
+        let expenses = [expense1, expense2];
+
+        expect(pipe.transform(expenses, "Ana")).toEqual([expense1]);
+   });
+});
