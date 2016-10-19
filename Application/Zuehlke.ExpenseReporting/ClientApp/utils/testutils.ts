947ca4ee02a6840bcba950076aa45e4495a76421
stanbe <stefan.benz@zuehlke.com>
2016-10-19 10:09:39 +0200

    add some testing utils

---
+++ Application/Zuehlke.ExpenseReporting/ClientApp/utils/testutils.ts
@@ -0,0 +1,28 @@
+﻿import { Injectable } from '@angular/core';
+import { NavigationExtras } from "@angular/router";
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+export class RouterStub {
+    navigateByUrl(url: string) { return url; }
+}
+
+@Injectable()
+export class ActivatedRouteStub {
+
+    // ActivatedRoute.params is Observable
+    private subject = new BehaviorSubject(this.testParams);
+    params = this.subject.asObservable();
+
+    // Test parameters
+    private _testParams: {};
+    get testParams() { return this._testParams; }
+    set testParams(params: {}) {
+        this._testParams = params;
+        this.subject.next(params);
+    }
+
+    // ActivatedRoute.snapshot.params
+    get snapshot() {
+        return { params: this.testParams };
+    }
+}
