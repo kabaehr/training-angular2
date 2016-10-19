e9daf94634170684b16049e0e0e8a79a0cbe36ba
stanbe <stefan.benz@zuehlke.com>
2016-10-19 11:55:57 +0200

    add explaining comment

--- Application/Zuehlke.ExpenseReporting/ClientApp/app/components/expense/pipes/expense-filter.pipe.spec.ts
+++ Application/Zuehlke.ExpenseReporting/ClientApp/app/components/expense/pipes/expense-filter.pipe.spec.ts
@@ -5,10 +5,10 @@ describe('ExpenseFilterPipe', () => {
    const pipe = new ExpenseFilterPipe();
 
    it('should filter out expenses that do not include <<Ana>>', () => {
-        let expense1 = new Expense('1', 'Anakin Skywalker', Reason.Hotel, "12-01-2016", 12.20, "Night on Coruscant with Obi Wan");
-        let expense2 = new Expense('1', 'Obi Wan', Reason.Hotel, "12-01-2016", 12.20, "Night on Coruscant with Anakin");
-        let expenses = [expense1, expense2];
+        const expense1 = new Expense('1', 'Anakin Skywalker', Reason.Hotel, "12-01-2016", 12.20, "Night on Coruscant with Obi Wan");
+        const expense2 = new Expense('1', 'Obi Wan', Reason.Hotel, "12-01-2016", 12.20, "Night on Coruscant with Anakin");
+        const expenses = [expense1, expense2];
 
-        expect(pipe.transform(expenses, "Ana")[0]).toEqual(expense1);
+        expect(pipe.transform(expenses, "Ana")[0]).toEqual(expense1); //check that only Anakin Skywalker is returned
    });
 });
