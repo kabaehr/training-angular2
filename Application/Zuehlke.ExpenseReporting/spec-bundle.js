89be7a2154d9e8ffca4adc79d39721b8e7d1c004
stanbe <stefan.benz@zuehlke.com>
2016-10-19 08:03:51 +0200

    add zone async test to spec-bundle

--- Application/Zuehlke.ExpenseReporting/spec-bundle.js
+++ Application/Zuehlke.ExpenseReporting/spec-bundle.js
@@ -18,6 +18,7 @@ require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone.js');
 require('zone.js/dist/proxy');
 require('zone.js/dist/sync-test');
+require('zone.js/dist/async-test');
 require('zone.js/dist/jasmine-patch.js');
 
 /*
