d90c92332cb8775ebcdcc6d73c7cd3b88b68ae5a
stanbe <stefan.benz@zuehlke.com>
2016-11-04 07:26:55 +0100

    Remove all expense code for list demo

--- Demos/ListDemo/Zuehlke.ExpenseReporting/ClientApp/app/components/expense/expense.routing.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
-
-
-export const productRoutes: Routes = [
-  
-];
-
-export const productRouting: ModuleWithProviders = RouterModule.forChild(productRoutes);
