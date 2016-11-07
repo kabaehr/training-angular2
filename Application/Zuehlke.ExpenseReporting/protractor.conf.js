8bc88471daf62b5d2ce43f1a6e812ad2e43e1ca5
Markus Rehrs <markus.rehrs@zuehlke.com>
2016-11-07 14:23:58 +0100

    renamed after folder of ex 2 to before

--- Application/Zuehlke.ExpenseReporting/protractor.conf.js
+++ Application/Zuehlke.ExpenseReporting/protractor.conf.js
@@ -1,6 +1,6 @@
 ﻿exports.config = {
     specs: ['**/*.e2e.spec.js'],
     useAllAngular2AppRoots: true,
-    baseUrl: 'http://localhost:54387/',
+    baseUrl: 'http://localhost:64065/',
     directConnect : true
 };
