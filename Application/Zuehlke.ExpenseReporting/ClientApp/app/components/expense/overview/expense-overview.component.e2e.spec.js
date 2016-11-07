27667e79ab406e90cb3c4484051ab39fd7893fc1
stanbe <stefan.benz@zuehlke.com>
2016-11-07 11:55:16 +0100

    add a protractor e2e test

---
+++ Application/Zuehlke.ExpenseReporting/ClientApp/app/components/expense/overview/expense-overview.component.e2e.spec.js
@@ -0,0 +1,11 @@
+﻿describe('Expense overview list', function () {
+
+    it('should should have Anakin Skywalker as first entry', function () {
+        browser.get('/home');
+        expect(browser.getTitle()).toEqual('Angular 2 Workshop');
+
+        browser.get('/overview');
+
+        expect(element(by.id('00000000-0000-0000-0000-000000000001')).getText()).toEqual('Anakin Skywalker');
+    });
+});
---
+++ Application/Zuehlke.ExpenseReporting/protractor.conf.js
@@ -0,0 +1,6 @@
+﻿exports.config = {
+    specs: ['**/*.e2e.spec.js'],
+    useAllAngular2AppRoots: true,
+    baseUrl: 'http://localhost:54387/',
+    directConnect : true
+};
